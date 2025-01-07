627ee06cacc882329ecf416441750d26cfd00e39
sergehould <houlds@vaniercollege.qc.ca>
2025-01-07 15:51:28 -0500

    Update config.h

--- pdcurses_test/mover4/header/config.h
+++ pdcurses_test/mover4/header/config.h
@@ -1,13 +1,24 @@
-/* SH					12 April 2023			*/
+/* SH					January 2025			*/
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define SIMULATION
+/* Enable the following macro run in REBEL4 mode.	*/
+/* Disable it to run in MOVER4 mode.				*/
+//#define REBEL4
+
 /* Enable the following macro run in UDP packet mode.	*/
-/* Disable it to run in simulation mode.				*/			
-//#define UDP
+/* Disable it to run in PCAN mode.				*/			
+#define UDP		
 
 /*	This is the IP address of the server.	*/
 /*	You must change this address			*/
-#define ADDRESS "192.168.0.111" // adress of the server
+#define ADDRESS "192.168.0.111" // adress of the server 
+//#define ADDRESS "192.168.0.112" // adress of the server
+//#define ADDRESS "192.168.0.113" // adress of the server
+//#define ADDRESS "192.168.0.114" // adress of the server
+//#define ADDRESS "192.168.0.115" // adress of the server
+//#define ADDRESS "192.168.0.116" // adress of the server
 
 #endif
+
