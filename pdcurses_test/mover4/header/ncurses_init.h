de2e07ae7a91f0cd51310416e9988f95f5d59fdb
sergehould <houlds@vaniercollege.qc.ca>
2025-01-07 16:01:00 -0500

    Update ncurses_init.h

--- pdcurses_test/mover4/header/ncurses_init.h
+++ pdcurses_test/mover4/header/ncurses_init.h
@@ -9,25 +9,35 @@
 
 #ifndef	NCURSE_INIT_H
 #define NCURSE_INIT_H
-#ifdef	__cplusplus
-extern "C" {
-#endif
+
 
 #define	WHITE_BLACK		COLOR_PAIR(1)
-#define	RED_WHITE		COLOR_PAIR(2)
-#define	GREEN_WHITE		COLOR_PAIR(3)
-#define	BLUE_WHITE		COLOR_PAIR(4)
-#define	RED_BLACK		COLOR_PAIR(5)
-#define	GREEN_BLACK		COLOR_PAIR(6)
-#define	BLUE_BLACK		COLOR_PAIR(7)
+#define	RED_BLACK		COLOR_PAIR(2)
+#define	GREEN_BLACK		COLOR_PAIR(3)
+#define	BLUE_BLACK		COLOR_PAIR(4)
+#define	YELLOW_BLACK	COLOR_PAIR(5)
+#define	MAGENTA_BLACK	COLOR_PAIR(6)
+#define	CYAN_BLACK		COLOR_PAIR(7)
+#define	BLACK_WHITE		COLOR_PAIR(8)
+#define	RED_WHITE		COLOR_PAIR(9)
+#define	GREEN_WHITE		COLOR_PAIR(10)
+#define	BLUE_WHITE		COLOR_PAIR(11)
+#define YELLOW_WHITE	COLOR_PAIR(12)
+#define	MAGENTA_WHITE	COLOR_PAIR(13)
+#define	CYAN_WHITE		COLOR_PAIR(14)
+#define	BLACK_YELLOW	COLOR_PAIR(15)
+#define	RED_YELLOW		COLOR_PAIR(16)
+#define	GREEN_YELLOW	COLOR_PAIR(17)
+#define	BLUE_YELLOW		COLOR_PAIR(18)
+#define	WHITE_YELLOW	COLOR_PAIR(19)
+#define	MAGENTA_YELLOW	COLOR_PAIR(20)
+#define	CYAN_YELLOW		COLOR_PAIR(21)
+
 
 /*Prototype Area*/
 void ncurses_init(void);
 int width_get(void);
 int height_get(void);
 void screen_init(void);
-#ifdef	__cplusplus
-}
-#endif
 
 #endif
